24912ca0422e80a8d8db892ce19607224f4f63d1
nikita-solovyov-dev <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-02-27 13:08:53 +0300

    Added logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import logging
-
-from flask import Flask
+from flask import Flask, request
 
 logging.basicConfig(
     level=logging.INFO,
@@ -14,18 +13,21 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-
-@app.route('/gpb/check-avail')
+@app.route('/gpb/check-avail', methods=['GET', 'POST'])
 def check_avail():
     logger.info("Received request on /gpb/check-avail")
+    logger.info("Request data: %s", request.data)
+    logger.info("Request args: %s", request.args)
+    logger.info("Request form: %s", request.form)
     return 'check_avail'
 
-
-@app.route('/gpb/payment-reg')
+@app.route('/gpb/payment-reg', methods=['GET', 'POST'])
 def payment_reg():
     logger.info("Received request on /gpb/payment-reg")
+    logger.info("Request data: %s", request.data)
+    logger.info("Request args: %s", request.args)
+    logger.info("Request form: %s", request.form)
     return 'payment_reg'
-
 
 if __name__ == '__main__':
     logger.info("Starting Flask app...")
